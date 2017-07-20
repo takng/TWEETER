85bd169285af807eb923ad2de86ca68f696b4002
TakNg <tak_ng@yahoo.com>
2017-07-20 06:27:18 +0000

    mongo

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,11 +5,6 @@
       tArea.val();
       counter.text(140 - tArea.val().length);
 
-      console.log(tArea.val());
-      console.log(this);
-      //console.log(this.val());
-      console.log(tArea.val().length);
-
       if (tArea.val().length > 140) {
         counter.addClass('over');
       } else {
