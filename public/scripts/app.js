9acb2212b698acb587760b28684b461e778b6129
TakNg <tak_ng@yahoo.com>
2017-07-20 05:06:03 +0000

    ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,166 +1,103 @@
- /*
-  * Client-side JS logic goes here
-  * jQuery is already loaded
-  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-  */
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
 
- const escape = (str) => {
-   var div = document.createElement('div');
-   div.appendChild(document.createTextNode(str));
-   return div.innerHTML;
- };
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
- const convertDate = (postedOn)  => {
-   let msPerMinute = 60 * 1000;
-   let msPerHour = msPerMinute * 60;
-   let msPerDay = msPerHour * 24;
-   let msPerMonth = msPerDay * 30;
-   let msPerYear = msPerDay * 365;
-   let elapsed = Date.now() - postedOn;
+function createTweetElement(tweet) {
+  let output = '';
 
-   if (elapsed < msPerMinute) {
-       return Math.round(elapsed/1000) + ' seconds ago';
-   }
-   else if (elapsed < msPerHour) {
-       return Math.round(elapsed/msPerMinute) + ' minutes ago';
-   }
-   else if (elapsed < msPerDay ) {
-       return Math.round(elapsed/msPerHour ) + ' hours ago';
-   }
-   else if (elapsed < msPerMonth) {
-      return Math.round(elapsed/msPerDay) + ' days ago';
-   }
-   else if (elapsed < msPerYear) {
-      return Math.round(elapsed/msPerMonth) + ' months ago';
-   }
-   else {
-      return Math.round(elapsed/msPerYear ) + ' years ago';
-   };
- };
+  output += `<article class="tweet">
+            <header>
+              <img src="${escape(tweet.user.avatars.small)}"/>
+              <h2>${escape(tweet.user.name)}</h2>
+              <span>${escape(tweet.user.handle)}</span>
+            </header>
+            <p>
+              ${escape(tweet.content.text)}
+            </p>
+            <footer>
+              <div class="new-tweet__time-stamp">${escape(tweet.created_at)}</div>
+              <div class="new-tweet__icons">
+                <i class="fa fa-flag" aria-hidden="true"></i>
+                <i class="fa fa-retweet" aria-hidden="true"></i>
+                <i class="fa fa-heart" aria-hidden="true"></i>
+              </div>
+            </footer>
+          </article>`
 
- const createTweetElement = (tweet) => {
-   "user": {
-     "name": "Newton",
-     "avatars": {
-       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-     },
-     "handle": "@SirIsaac"
-   },
-   "content": {
-     "text": "If I have seen further it is by standing on the shoulders of giants"
-   },
-   "created_at": 1461116232227
- };
+  return output
+}
 
- // new tweets template
- // const createTweetElement = (tweet) => `<article class="tweet">
- //   <header>
- //     <img src="${escape(tweet.user.avatars.small)}"/>
- //     <h2>${escape(tweet.user.name)}</h2>
- //     <span>${escape(tweet.user.handle)}</span>
- //   </header>
- //   <p>
- //     ${escape(tweet.content.text)}
- //   </p>
- //   <footer>
- //     <div class="new-tweet__time-stamp">${escape(convertDate(tweet.created_at))}</div>
- //     <div class="new-tweet__icons">
- //       <i class="fa fa-flag" aria-hidden="true"></i>
- //       <i class="fa fa-retweet" aria-hidden="true"></i>
- //       <i class="fa fa-heart" aria-hidden="true"></i>
- //     </div>
- //   </footer>
- // </article>`;
+function renderTweets(tweets) {
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  for (let tweetData of tweets) {
+    let $tweet = createTweetElement(tweetData);
+    $('.new-tweet__container').prepend($tweet);
+  };
+};
 
- const renderTweets = (tweets) => {
-   for (let tweetData of tweets) {
-     let $tweet = createTweetElement(tweetData);
-     $('.new-tweet__container').prepend($tweet);
-   };
- };
+$(document).ready(function() {
 
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets'
+    })
+    .done((data) => {
+      renderTweets(data);
+    })
+    .fail(console.error);
+  };
 
- // begin jQuery functions on document ready
- $(document).ready(function() {
+  $('.compose').click(function(ev) {
+    $('.new-tweet').slideToggle('200');
+    $('.textarea').focus();
+  });
 
-   $('#nav-bar .logo, #nav-bar .header').on('click', () => {
-     location.reload(true);
-   });
+  $('.new-tweet__container').on('mouseenter', '.tweet', function() {
+    $(this).find('.new-tweet__icons').addClass('show');
+  })  
 
-   const loadTweets = () => {
-     $.ajax({
-       method: 'GET',
-       url: '/tweets'
-     })
-     .then((data) => {
-       renderTweets(data);
-     }, console.error);
-   };
+  $('.new-tweet__container').on('mouseleave', '.tweet', function() {
+    $(this).find('.new-tweet__icons').removeClass('show');
+  })
 
+  // form handlers
+  $('.new-tweet form').on('submit', (e) => {
+    e.preventDefault();
 
-   // load initial tweets
-   loadTweets();
+    let textBox = $('.new-tweet textarea');
 
+    if (textBox.val() === '') {
+      alert(`Empty input!`);
+      return;
+    } else if (textBox.val().length > 140) {
+      alert(`Tweet more than 140 characters!`);
+      return;
+    }
 
-   // social icons
-   let $tweetContainer = $('.new-tweet__container');
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: $('.new-tweet form').serialize()
+    })
+    .then((new_tweet) => {
+      loadTweets();
+      textBox.val('');
+      $(this).find('.counter').text('140');
 
-   $tweetContainer.on('mouseenter', '.tweet', function() {
-     $(this).find('.new-tweet__icons').addClass('show');
-   })
-   $tweetContainer.on('mouseleave', '.tweet', function() {
-     $(this).find('.new-tweet__icons').removeClass('show');
-   });
+    })
+  });
 
+  loadTweets();
+});
 
-   // toggle compose form
-   let $composeBox = $('.new-tweet');
-
-   $('.compose').on('click', () => {
-     $composeBox.slideToggle('slow').focus();
-
-     if($composeBox.is(':visible')) {
-       $composeBox.find('textarea').focus();
-     };
-   });
-
-
-   // append new tweet submissions and store in database
-   let $newTweet = $('.new-tweet form');
-
-   $newTweet.on('submit', (e) => {
-     e.preventDefault();
-
-     let $tweetBox = $('.new-tweet textarea');
-     let $tweetBoxError = $('.tweet-append-error');
-
-     if ($tweetBox.val() === '') {
-       $tweetBoxError.remove();
-       $newTweet.append(`<span class="tweet-append-error">
-         Oops! The tweet field is empty!
-       </span>`);
-       return;
-     } else if ($tweetBox.val().length > 140) {
-       $tweetBoxError.remove();
-       $newTweet.append(`<span class="tweet-append-error">
-         Oops! Your tweet is too long!
-       </span>`);
-       return;
-     };
-
-     $.ajax({
-       method: 'POST',
-       url: '/tweets',
-       data: $newTweet.serialize()
-     })
-     .then((new_tweet) => {
-       $tweetBoxError.remove();
-       loadTweets();
-       $tweetBox.val('');
-       $newTweet.find('.counter').text('140');
-     });
-   });
-
- });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,6 @@
-// var script = document.createElement("SCRIPT");
-// script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
-// script.type = 'text/javascript';
-//
-// $(document).ready(function(){
-//   console.log(document);
-  // console.log(document.body);
-  // // console.log( $( "body" ).data( "textarea" ) );
-  // console.log (("selector").data);
-
   $(document).ready(function() {
     let tArea = $('textarea');
     let counter = $('.counter');
-console.log(tArea.val());
-console.log(tArea.val().length);
     $('.new-tweet').on('keyup', 'textarea', function() {
       tArea.val();
       counter.text(140 - tArea.val().length);
@@ -29,32 +17,4 @@ console.log(tArea.val().length);
       }
     });
   });
-  //console.log(document.textarea.newTweet);
-    //
-    // $("button").click(function(){
-    //     $("p").hide();
-    //     console.log(document);
-    // });
 
-    // document.click(function(){
-    //     //$("p").hide();
-    //     console.log(document);
-    // });
-    //
-    // $("textarea.newTweet").input(function() {
-    //     console.log(document);
-//       $($0).toggleClass('over')
-// [span.counter, context: span.counter]
-// $($0).toggleClass('over')
-// [span.counter.over, context: span.counter.over]
-// $($0).toggleClass('over')
-// [span.counter, context: span.counter]
-      // get the length of the textarea's value (the text the user inputted)
-
-      // update our character counter html to show user how many characters
-      // they have left
-
-    //   Toggle a class on the character counter to notify the user they are
-    //   over the limit
-    // });
-//});
